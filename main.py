0a9a50af84759715bd99ef875df7d3ac1f67e9e6
daruoshazadi333-ux <daruosh.azadi333@gmail.com>
2026-09-02 13:02:51 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-imp@#ort asyncio, json, random, logging, traceback, time
+import asyncio, json, random, logging, traceback, time
 from pathlib import Path
 from datetime import datetime, date, timedelta
 from os import system as os_system, name as os_name
